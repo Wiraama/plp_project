5ba36e1aa49e270c16321381ce5e1fad1fb19201
Wiriama <mawira01@students.uonbi.ac.ke>
2024-03-08 09:16:40 +0300

    week two assignment

---
+++ python/list.py
@@ -0,0 +1,9 @@
+my_list = []
+my_list.extend([10, 20, 30, 40])
+my_list.insert(1, 15)
+list2 = [50, 60, 70]
+my_list.extend(list2)
+my_list.pop()
+my_list.sort()
+#print(my_list)
+print(my_list.index(30))
